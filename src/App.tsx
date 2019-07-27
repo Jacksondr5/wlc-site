5753aebac7699230be7baa64512fda3378f0c6a0
Jackson Miller <jacksonmiller7855@gmail.com>
2019-07-27 17:42:15 -0400

    added challenge form and foundational UI work

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider } from '@material-ui/styles';
+import NewChallengeForm from './components/NewChallengeForm';
+import ResponsiveDrawer from './components/ResponsiveDrawer';
+import { CssBaseline, Container, createMuiTheme } from '@material-ui/core';
 
 const App: React.FC = () => {
+  const theme = createMuiTheme({
+    spacing: 2
+  });
+  // const isiOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+  //TODO: Consider using a maxWidth on the container to simplify development
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container>
+        <ResponsiveDrawer>
+          <NewChallengeForm />
+        </ResponsiveDrawer>
+      </Container>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
--- src/components/NewChallengeForm.tsx
+++ src/components/NewChallengeForm.tsx
@@ -1,7 +1,101 @@
 import React from 'react';
+import {
+  TextField,
+  InputAdornment,
+  Button,
+  Grid,
+  Typography
+} from '@material-ui/core';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker
+} from '@material-ui/pickers';
+import moment, { Moment } from 'moment';
+import MomentUtils from '@date-io/moment';
+import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
 
-// class NewChallengeForm extends React.Component<{
-//     constructor(parameters) {
+interface State {
+  name: String;
+  entryFee: Number;
+  startDate: Moment;
+  endDate: Moment;
+}
 
-//     }
-// }
+export default function NewChallengeForm() {
+  const [state, setState] = React.useState<State>({
+    name: '',
+    entryFee: 0,
+    startDate: moment(),
+    endDate: moment().add(4, 'weeks')
+  });
+  const handleChange = (name: keyof State) => (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setState({ ...state, [name]: event.target.value });
+  };
+  const handleDatechange = (name: keyof State) => (
+    date: MaterialUiPickersDate,
+    value?: string | null | undefined
+  ) => setState({ ...state, [name]: date });
+  return (
+    <form noValidate autoComplete="off">
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <Typography variant="h5">New Challenge</Typography>
+        </Grid>
+        <Grid item xs={12} lg={6}>
+          <TextField
+            id="challengeName"
+            label="Challenge Name"
+            value={state.name}
+            onChange={handleChange('name')}
+            margin="normal"
+            fullWidth
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={12} lg={6}>
+          <TextField
+            id="entry-fee"
+            label="Entry Fee"
+            value={state.entryFee}
+            onChange={handleChange('entryFee')}
+            margin="normal"
+            fullWidth
+            variant="outlined"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">$</InputAdornment>
+              )
+            }}
+          />
+        </Grid>
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          <Grid item xs={6}>
+            <KeyboardDatePicker
+              margin="normal"
+              id="start-date-picker"
+              label="Start Date"
+              fullWidth
+              value={state.startDate}
+              onChange={handleDatechange('startDate')}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <KeyboardDatePicker
+              margin="normal"
+              id="end-date-picker"
+              label="End Date"
+              fullWidth
+              value={state.endDate}
+              onChange={handleDatechange('endDate')}
+            />
+          </Grid>
+        </MuiPickersUtilsProvider>
+        <Button variant="contained" color="primary">
+          Submit
+        </Button>
+      </Grid>
+    </form>
+  );
+}
